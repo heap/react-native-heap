b50189b2b794999b02d53c041a3e72422ee398db
James Taber <jmtaber129@gmail.com>
2019-01-22 16:05:04 -0800

    CR

--- instrumentor/src/index.js
+++ instrumentor/src/index.js
@@ -35,34 +35,36 @@ function transform (babel) {
     visitor: {
       ObjectProperty(path) {
         if (path.node.key.name === 'touchableHandlePress' || path.node.key.name === 'touchableHandleLongPress') {
-          const parent = path.findParent(path => path.isCallExpression() && path.node.callee.name === 'createReactClass');
-          if (parent) {
-            if (!hasTouchableMixin(path)) {
-              return;
-            }
+          const parent = path.findParent(path => {
+            return path.isCallExpression() && path.node.callee.name === 'createReactClass';
+          });
 
-            // Create the expression for calling the original function for this listener.
-            // '(<original function>).call(this, e)'.
-            const originalFunctionExpression = path.node.value;
-            const callOriginalFunctionExpression = t.memberExpression(originalFunctionExpression, t.identifier('call'));
-            const calledFunction = t.callExpression(callOriginalFunctionExpression, [t.identifier('this'), t.identifier('e')]);
-
-            // Create the expression for calling Heap autotrack.
-            // 'Heap.autotrackPress(<press type>, this, e)'.
-            const autotrackExpression = t.callExpression(
-              t.memberExpression(t.identifier('Heap'), t.identifier('autotrackPress')),
-              [t.stringLiteral(path.node.key.name), t.identifier('this'), t.identifier('e')]);
-
-            // Function body for tracking the Heap event, then calling the original function).
-            const functionBody = buildFunctionWrapper({
-              AUTOTRACK_EXPRESSION: autotrackExpression,
-              ORIGINAL_FUNCTION_CALL: calledFunction,
-            });
-
-            // Call the function body with the event parameter 'e'.
-            const replacementFunc = t.arrowFunctionExpression([t.identifier('e')], functionBody);
-            path.get('value').replaceWith(replacementFunc);
+          if (!(parent && hasTouchableMixin(path))) {
+            return;
           }
+
+          // Create the expression for calling the original function for this listener.
+          // '(<original function>).call(this, e)'.
+          const originalFunctionExpression = path.node.value;
+          const callOriginalFunctionExpression = t.memberExpression(originalFunctionExpression, t.identifier('call'));
+          const calledFunction = t.callExpression(callOriginalFunctionExpression, [t.identifier('this'), t.identifier('e')]);
+
+          // Create the expression for calling Heap autotrack. Pass 'this' so we have the component
+          // context to extract the hierarchy, and 'e' so we have context on the event.
+          // 'Heap.autotrackPress(<press type>, this, e)'.
+          const autotrackExpression = t.callExpression(
+            t.memberExpression(t.identifier('Heap'), t.identifier('autotrackPress')),
+            [t.stringLiteral(path.node.key.name), t.identifier('this'), t.identifier('e')]);
+
+          // Function body for tracking the Heap event, then calling the original function.
+          const functionBody = buildFunctionWrapper({
+            AUTOTRACK_EXPRESSION: autotrackExpression,
+            ORIGINAL_FUNCTION_CALL: calledFunction,
+          });
+
+          // Call the function body with the event parameter 'e'.
+          const replacementFunc = t.arrowFunctionExpression([t.identifier('e')], functionBody);
+          path.get('value').replaceWith(replacementFunc);
         }
       }
     }
