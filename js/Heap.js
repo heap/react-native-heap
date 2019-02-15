5d65512a46dcab13c9afd1f240b76db95e1f14ac
James Taber <jmtaber129@gmail.com>
2019-02-15 20:19:35 +0100

    Extract from memoized props

--- js/Heap.js
+++ js/Heap.js
@@ -118,7 +118,7 @@ const getFiberNodeComponentHierarchy = currNode => {
 
   const propsString = extractProps(
     elementName,
-    currNode.stateNode,
+    currNode,
     builtinPropExtractorConfig
   );
 
