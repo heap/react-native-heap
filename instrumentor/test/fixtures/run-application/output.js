7f00abe766eefdc3f96b9cd93bd41e968920d90d
Andy Isaacson <andyi@heapanalytics.com>
2019-03-18 12:56:13 -0500

    Add a log message at startup if app has successfully been instrumented

---
+++ instrumentor/test/fixtures/run-application/output.js
@@ -0,0 +1,8 @@
+var AppRegistry = {
+  runApplication: function runApplication(appKey, appParameters) {
+    infoLog('Heap: Touchables are instrumented for autocapture.');
+    (function (appKey, appParameters) {
+      var foo = 'bar';
+    }).call(this, appKey, appParameters);
+  }
+};
