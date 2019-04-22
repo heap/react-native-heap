700fb9176d6c07debcbbb79355e858177d6aae58
Timothy Andrew <mail@timothyandrew.net>
2019-04-22 13:21:18 +0530

    [HEAP-8587] Fix sessionization for the `TestDriver` android app.
    It turns out that `Heap.init` is called (from Javascript) too late,
    after `MainActivity` is created. This leads our library to assume that
    `MainActivity` is backgrounded, and so all events are treated as custom
    events, with each receiving a different session ID.
    
    The fix is simple; call `Heap.init` from `MainActivity` directly (in
    Java), before the Javascript is loaded.

--- examples/TestDriver/android/app/src/main/java/com/testdriver/MainActivity.java
+++ examples/TestDriver/android/app/src/main/java/com/testdriver/MainActivity.java
@@ -1,8 +1,15 @@
 package com.testdriver;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import com.heapanalytics.reactnative.RNHeap;
 
 public class MainActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        RNHeap.init(getApplicationContext(), "11");
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
