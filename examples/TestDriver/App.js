148a7016135692a3a248ae2ad58a717878346588
James Taber <jmtaber129@gmail.com>
2019-06-06 16:56:33 -0700

    Fix ref forwarding

--- examples/TestDriver/App.js
+++ examples/TestDriver/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { NavigationActions } from 'react-navigation';
 import { store } from './src/reduxElements';
 import TopNavigator from './src/topNavigator';
+
+let _navigator;
+
+function setTopLevelNavigator(navigatorRef) {
+  _navigator = navigatorRef;
+
+  navigate('Initial');
+}
+
+function navigate(routeName, params) {
+  console.log(_navigator);
+  _navigator.dispatch(
+    NavigationActions.navigate({
+      routeName,
+      params,
+    })
+  );
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <TopNavigator />
+        <TopNavigator ref={setTopLevelNavigator} />
       </Provider>
     );
   }
