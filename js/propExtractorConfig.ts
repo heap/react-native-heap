8869336d45e7a293730f849a07a36d6bca34bd99
James Taber <jmtaber129@gmail.com>
2019-06-13 13:40:25 -0700

    Move library-specific props to separate directory

--- js/propExtractorConfig.ts
+++ js/propExtractorConfig.ts
@@ -1,6 +1,7 @@
 import * as _ from 'lodash';
 
 import { PropExtractorConfig } from './util/extractProps';
+import { REACT_NATIVE_ELEMENTS_CONFIG } from './libraryPropConfigs/rnElementsPropConfig';
 
 const BASE_CONFIG: PropExtractorConfig = {
   '*': {
@@ -9,77 +10,6 @@ const BASE_CONFIG: PropExtractorConfig = {
   },
   Button: {
     include: ['title'],
-    exclude: [],
-  },
-};
-
-const REACT_NATIVE_ELEMENTS_CONFIG: PropExtractorConfig = {
-  // Interactive components.
-  Avatar: {
-    include: [
-      'editButton.name',
-      'editButton.type',
-      'icon.name',
-      'icon.type',
-      'showEditButton',
-      'title',
-    ],
-    exclude: [],
-  },
-  Badge: {
-    include: ['status', 'value'],
-    exclude: [],
-  },
-  Button: {
-    include: [
-      'icon.name',
-      'icon.type',
-      'iconRight',
-      'loading',
-      'type',
-      'disabled',
-    ],
-    exclude: [],
-  },
-  CheckBox: {
-    include: ['iconType', 'title', 'checkedTitle'],
-    exclude: [],
-  },
-  Icon: {
-    include: ['name', 'type'],
-    exclude: [],
-  },
-  Input: {
-    include: ['errorMessage', 'label', 'placeholder'],
-    exclude: [],
-  },
-  ListItem: {
-    include: ['title', 'subtitle', 'rightTitle', 'rightSubtitle'],
-    exclude: [],
-  },
-  PricingCard: {
-    include: ['price', 'title', 'button.title'],
-    exclude: [],
-  },
-  TapRating: {
-    include: ['count'],
-    exclude: [],
-  },
-  SearchBar: {
-    include: ['platform', 'placeholder'],
-    exclude: [],
-  },
-  SocialIcon: {
-    include: ['button', 'type', 'title'],
-    exclude: [],
-  },
-  Tile: {
-    include: ['caption', 'featured', 'icon.name', 'icon.type', 'title'],
-    exclude: [],
-  },
-  // Non-interactive components.
-  Card: {
-    include: ['featuredSubtitle', 'featuredTitle', 'title'],
     exclude: [],
   },
 };
