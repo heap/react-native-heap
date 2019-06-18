f9e45caad539995fd109ab568dcb2ccabe7ed847
James Taber <jmtaber129@gmail.com>
2019-06-18 16:14:25 -0700

    Exclude target text

--- js/autotrack/scrollViews.js
+++ js/autotrack/scrollViews.js
@@ -1,4 +1,5 @@
 import { getBaseComponentProps } from './common';
+import * as _ from 'lodash';
 
 export const autotrackScrollView = track => (eventType, componentThis, event) => {
   if (!componentThis.props.pagingEnabled) {
@@ -8,7 +9,9 @@ export const autotrackScrollView = track => (eventType, componentThis, event) =>
     return;
   }
 
-  const autotrackProps = getBaseComponentProps(componentThis);
+  // Target text in a scrollview will be the entire contents of the scrollview, which isn't
+  // particularly meaningful. Just leave out the target text.
+  const autotrackProps = _.omit(getBaseComponentProps(componentThis), 'targetText');
 
   if (!autotrackProps) {
     // We're not capturing this interaction.
