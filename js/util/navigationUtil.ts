d80710c557c2fa18eaf959324da645b2b1000684
James Taber <jmtaber129@gmail.com>
2019-07-18 10:44:17 -0700

    Fix lint

--- js/util/navigationUtil.ts
+++ js/util/navigationUtil.ts
@@ -32,21 +32,20 @@ export default class NavigationUtil {
     const paths = this.getActiveRouteNames(navigationState);
     return {
       path: paths.join('::'),
-      screenName: paths[paths.length - 1] };
+      screenName: paths[paths.length - 1],
+    };
   }
 
   // Returns an array of route names, with the root name first, and the most nested name last.
-  private static getActiveRouteNames(
-    navigationState: any
-  ): Array<string> {
+  private static getActiveRouteNames(navigationState: any): Array<string> {
     const route = navigationState.routes[navigationState.index];
 
     // Dive into nested navigators.
     if (route.routes) {
       const paths = this.getActiveRouteNames(route);
-      return [ route.routeName ].concat(paths);
+      return [route.routeName].concat(paths);
     }
 
-    return [ route.routeName ];
+    return [route.routeName];
   }
 }
