6e2756a47fe58df281e13bf60540de64bbf1c30d
James Taber <jmtaber129@gmail.com>
2019-08-21 16:57:46 -0700

    Fix build when dev or prod is undefined

--- ios/HeapSettings.bundle/generate_settings_common.rb
+++ ios/HeapSettings.bundle/generate_settings_common.rb
@@ -16,16 +16,16 @@ end
 def get_heap_settings_from_json(json)
   dev, prod, default = json.values_at('dev', 'prod', 'default')
 
-  auto_init_dev = dev['heapAutoInit']
+  auto_init_dev = dev['heapAutoInit'] unless dev.nil?
   auto_init_dev = default['heapAutoInit'] if auto_init_dev.nil?
   auto_init_dev = true if auto_init_dev.nil?
 
-  auto_init_prod = prod['heapAutoInit']
+  auto_init_prod = prod['heapAutoInit'] unless prod.nil?
   auto_init_prod = default['heapAutoInit'] if auto_init_prod.nil?
   auto_init_prod = true if auto_init_prod.nil?
 
-  app_id_dev = dev['heapAppId'] || default['heapAppId'] if auto_init_dev
-  app_id_prod = prod['heapAppId'] || default['heapAppId'] if auto_init_prod
+  app_id_dev = (!dev.nil? && dev['heapAppId']) || default['heapAppId'] if auto_init_dev
+  app_id_prod = (!prod.nil? && prod['heapAppId']) || default['heapAppId'] if auto_init_prod
 
   return app_id_dev, app_id_prod
 end
