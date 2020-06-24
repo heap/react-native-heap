855163cc0d2f6ef14e0ea4138dfc83d0300e9c78
James Taber <jmtaber129@gmail.com>
2020-06-24 13:58:48 -0600

    Add RN 0.62 test app

---
+++ examples/TestDriverRN062/babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    "add-react-displayname",
+    "./node_modules/@heap/react-native-heap/instrumentor/src/index.js"
+  ]
+};
