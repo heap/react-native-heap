8f03338e01c154dc6ce524f7c3f038d2bc5a72bf
James Taber <jmtaber129@gmail.com>
2020-07-30 18:33:40 -0500

    Add React Navigation 5 and add screens

---
+++ examples/TestDriverRN063/src/screens/TouchablesScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button, TouchableOpacity, ScrollView, Text } from 'react-native';
+
+export const TouchablesScreen = () => {
+  return (
+    <ScrollView>
+      <Button title='Press me' onPress={() => console.log('pressed button')} />
+      <TouchableOpacity onPress={() => console.log('pressed touchable opacity')}>
+        <Text>Touchable Opacity</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
+}
