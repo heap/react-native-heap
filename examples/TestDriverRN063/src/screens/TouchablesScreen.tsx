ed6d0b8566e1ed955b0c36bceeabd75fbedfda70
James Taber <jmtaber129@gmail.com>
2020-08-04 18:23:03 -0500

    Add TextInput component to new test app

--- examples/TestDriverRN063/src/screens/TouchablesScreen.tsx
+++ examples/TestDriverRN063/src/screens/TouchablesScreen.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
-import { Button, TouchableOpacity, ScrollView, Text } from 'react-native';
+import { Button, TouchableOpacity, ScrollView, Text, TextInput } from 'react-native';
+
+const MyTextInput = () => {
+  const [value, onChangeText] = React.useState();
+
+  return (
+    <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => onChangeText(text)}
+      value={value}
+    />
+  );
+}
 
 export const TouchablesScreen = () => {
   return (
@@ -9,6 +21,7 @@ export const TouchablesScreen = () => {
         onPress={() => console.log('pressed touchable opacity')}>
         <Text>Touchable Opacity</Text>
       </TouchableOpacity>
+      <MyTextInput />
     </ScrollView>
   );
 };
