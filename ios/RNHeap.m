60193f372dc986b8a2e399143ec2af0cec3abbb2
Brian Nickel <brian.nickel@heap.io>
2022-03-29 13:02:50 -0700

    Add logic to delay pageviews when Event Visualizer is paired

--- ios/RNHeap.m
+++ ios/RNHeap.m
@@ -4,6 +4,8 @@
 
 #import "RNHeap.h"
 #import <Heap/Heap.h>
+#import <Foundation/Foundation.h>
+#import <objc/message.h>
 
 #if __has_include("RCTBridge.h")
 #import "RCTBridge.h"
@@ -17,6 +19,17 @@
 #import <React/RCTDevMenu.h>
 #endif
 
+@interface RNHeap ()
+@property (nonatomic, assign) BOOL delayPageviewsUntilViewDidAppear;
++ (void)consumeDelayedPageview;
+@end
+
+typedef void (*IMP_ViewDidAppear)(UIViewController *, SEL, BOOL);
+static IMP_ViewDidAppear __RNHEAP_ViewDidAppear_Orig;
+void __RNHEAP_ViewDidAppear(UIViewController *self, SEL _cmd, BOOL animated) {
+    __RNHEAP_ViewDidAppear_Orig(self, _cmd, animated);
+    [RNHeap consumeDelayedPageview];
+}
 
 @implementation RNHeap
 
@@ -26,7 +39,43 @@ static BOOL pageViewSent = NO;
 static BOOL appIdSet = NO;
 
 - (dispatch_queue_t)methodQueue {
-  return dispatch_get_main_queue();
+    return dispatch_get_main_queue();
+}
+
+static void(^delayedPageview)(void);
+
+/// This function delays a pageview until we think the animation has completed.
+/// Since the state change fires at the start of the animation, we wait half a second for the transiton to complete.
+/// If they're using native UIViewController transitions, we can detect the actual end of the animation with `viewDidAppear:`.
++ (void)scheduleDelayedPageview:(void(^)(void))block
+{
+    [self consumeDelayedPageview];
+    delayedPageview = [block copy];
+    double delayInSeconds = 0.5;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [self consumeDelayedPageview];
+    });
+}
+
++ (void)consumeDelayedPageview
+{
+    if (delayedPageview) {
+        delayedPageview();
+        delayedPageview = nil;
+    }
+}
+
++ (void)swizzleViewDidAppear
+{
+    // TODO: Is there a way to track the end of React Native animations?
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Method method = class_getInstanceMethod([UIViewController class], @selector(viewDidAppear:));
+        if (method) {
+            __RNHEAP_ViewDidAppear_Orig = (IMP_ViewDidAppear)method_setImplementation(method, (IMP)__RNHEAP_ViewDidAppear);
+        }
+    });
 }
 
 RCT_EXPORT_METHOD(setAppId:(NSString *)appId) {
@@ -44,15 +93,20 @@ RCT_EXPORT_METHOD(setAppId:(NSString *)appId) {
 }
 
 RCT_EXPORT_METHOD(autocaptureEvent:(NSString *)event withProperties:(NSDictionary *)properties) {
-  [self checkForPageview];
-
-  [Heap frameworkAutocaptureEvent:event withSource:@"react_native" withSourceProperties:properties];
+    [self checkForPageview];
+    
+    if (self.delayPageviewsUntilViewDidAppear && [event isEqualToString:@"react_navigation_screenview"]) {
+        [RNHeap scheduleDelayedPageview:^{
+            [Heap frameworkAutocaptureEvent:event withSource:@"react_native" withSourceProperties:properties];
+        }];
+    } else {
+        [Heap frameworkAutocaptureEvent:event withSource:@"react_native" withSourceProperties:properties];
+    }
 }
 
 RCT_EXPORT_METHOD(manuallyTrackEvent:(NSString *)event withProperties:(NSDictionary *)properties withContext:(NSDictionary *)contextProperties) {
-  [self checkForPageview];
-
-  [Heap frameworkTrack:event withProperties:properties withSource:@"react_native" withSourceProperties:contextProperties];
+    [self checkForPageview];
+    [Heap frameworkTrack:event withProperties:properties withSource:@"react_native" withSourceProperties:contextProperties];
 }
 
 -(void) checkForPageview {
@@ -74,40 +128,41 @@ RCT_EXPORT_METHOD(manuallyTrackEvent:(NSString *)event withProperties:(NSDiction
 }
 
 RCT_REMAP_METHOD(getUserId, getUserIdWithResolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
-  NSString *userId = [Heap userId];
-  resolve(userId);
+    NSString *userId = [Heap userId];
+    resolve(userId);
 }
 
 RCT_EXPORT_METHOD(identify:(NSString *)identity) {
-  [Heap identify:identity];
+    [Heap identify:identity];
 }
 
 RCT_EXPORT_METHOD(resetIdentity) {
-  [Heap resetIdentity];
+    [Heap resetIdentity];
 }
 
 RCT_EXPORT_METHOD(addUserProperties:(NSDictionary *)properties) {
-  [Heap addUserProperties:properties];
+    [Heap addUserProperties:properties];
 }
 
 RCT_EXPORT_METHOD(addEventProperties:(NSDictionary *)properties) {
-  [Heap addEventProperties:properties];
+    [Heap addEventProperties:properties];
 }
 
 RCT_EXPORT_METHOD(removeEventProperty:(NSString *)property) {
-  [Heap removeEventProperty:property];
+    [Heap removeEventProperty:property];
 }
 
 RCT_EXPORT_METHOD(clearEventProperties) {
-  [Heap clearEventProperties];
+    [Heap clearEventProperties];
 }
 
-- (void)setBridge:(RCTBridge *)bridge
-{
+- (void)setBridge:(RCTBridge *)bridge {
     _bridge = bridge;
     
-    [bridge.devMenu addItem:[RCTDevMenuItem buttonItemWithTitle:@"Pair Event Visualizer" handler:^{
+    [bridge.devMenu addItem:[RCTDevMenuItem buttonItemWithTitle:@"Pair Heap Event Visualizer" handler:^{
         [Heap startEVPairing];
+        self.delayPageviewsUntilViewDidAppear = YES;
+        [RNHeap swizzleViewDidAppear];
     }]];
 }
 
