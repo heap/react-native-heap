509ebbcc280683a695a3646b791eaed052a49fbc
Brian Nickel <brian.nickel@heap.io>
2022-04-05 18:11:05 -0700

    Use synthetic pageviews from Heap 8.2 and combine init methods

--- ios/RNHeap.m
+++ ios/RNHeap.m
@@ -3,62 +3,27 @@
 //
 
 #import "RNHeap.h"
+#import "RNHeapInit.h"
 #import <Heap/Heap.h>
-
 
 @implementation RNHeap
 
 RCT_EXPORT_MODULE();
-
-static BOOL pageViewSent = NO;
-static BOOL appIdSet = NO;
 
 - (dispatch_queue_t)methodQueue {
   return dispatch_get_main_queue();
 }
 
 RCT_EXPORT_METHOD(setAppId:(NSString *)appId) {
-    // The Heap library stops sending events if setAppId is called twice, which
-    // is often the case if you reload javascript during development.  We should
-    // fix that behavior in the library, but this will prevent the issue until
-    // that's updated.
-    // TODO: Remove this check when the iOS tracker allows setAppId to be called multiple times.
-    if (!appIdSet) {
-        [Heap initialize:appId];
-        appIdSet = YES;
-    } else {
-        NSLog(@"The appId was already set - ignoring repeated call.");
-    }
+    [RNHeapInit initializeWithAppId:appId enableTouchAutocapture:NO enableDebugLogging:NO captureBaseUrl:nil];
 }
 
 RCT_EXPORT_METHOD(autocaptureEvent:(NSString *)event withProperties:(NSDictionary *)properties) {
-  [self checkForPageview];
-
   [Heap frameworkAutocaptureEvent:event withSource:@"react_native" withSourceProperties:properties];
 }
 
 RCT_EXPORT_METHOD(manuallyTrackEvent:(NSString *)event withProperties:(NSDictionary *)properties withContext:(NSDictionary *)contextProperties) {
-  [self checkForPageview];
-
   [Heap frameworkTrack:event withProperties:properties withSource:@"react_native" withSourceProperties:contextProperties];
-}
-
--(void) checkForPageview {
-    // The Heap library requires that a "page view" event be sent first, since properties
-    // will get copied down to manual events.  Unfortunately, the first page view happens
-    // before any JS code is run, and so the app doesn't yet have an ID.  This unfortunate
-    // snippet makes sure that a page view gets sent first.
-    #pragma clang diagnostic push
-    #pragma clang diagnostic ignored "-Wundeclared-selector"
-    #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-    if (!pageViewSent) {
-        SEL logPageviewSelector = @selector(logPageview:);
-        if ([[Heap class] respondsToSelector:logPageviewSelector]) {
-            [[Heap class] performSelector:logPageviewSelector withObject:@{@"type": @"react-native"}];
-        }
-        pageViewSent = YES;
-    }
-    #pragma clang diagnostic pop
 }
 
 RCT_REMAP_METHOD(getUserId, getUserIdWithResolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
