18bafca58bb048b1868e8306666706c589ecabb8
Brian Nickel <brian.nickel@heap.io>
2023-08-18 14:26:55 -0700

    Rename test fixture

--- instrumentor/test/index.js
+++ instrumentor/test/index.js
@@ -37,8 +37,8 @@ describe('autotrack instrumentor plugin', () => {
   });
 
   it('switch should be instrumented', () => {
-    var actual = getActualTransformedFile('is-switch');
-    var expected = getExpectedTransformedFile('is-switch');
+    var actual = getActualTransformedFile('is-switch-pre-0-66');
+    var expected = getExpectedTransformedFile('is-switch-pre-0-66');
     assert.equal(actual, expected);
   });
 
